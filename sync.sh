718d0f112cd49f738146359415bc279963f2a7ad
Dan Robert <danielwrobert@gmail.com>
2018-03-26 23:47:35 -0400

    Added update script and adjusted exclude files.

---
+++ sync.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doUpdate() {
+	rsync --exclude ".git/" \
+		--exclude "init" \
+		--exclude "bin" \
+		--exclude ".DS_Store" \
+		--exclude ".macos" \
+		--exclude ".vimrc_remote" \
+		--exclude "bootstrap.sh" \
+		--exclude "brew.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doUpdate;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doUpdate;
+	fi;
+fi;
+unset doUpdate;
